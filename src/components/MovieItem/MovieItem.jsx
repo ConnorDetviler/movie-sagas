15230fae1bfb09af32a48d23d21c8c46b1442870
Connor Detviler <connor.detviler@gmail.com>
2021-01-31 16:29:57 -0600

    turned movie posters into materialUI cards

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -2,8 +2,26 @@ import './MovieItem.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from "react-router-dom";
 
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 300,
+      width: 200
+    },
+});
 
 function MovieItem({movie}) {
+    const classes = useStyles();
 
     const history = useHistory();
     const dispatch = useDispatch();
@@ -15,10 +33,19 @@ function MovieItem({movie}) {
 
 
     return (
-        <div className="movie-card" onClick={toDetails}>
-            <h3>{movie.title}</h3>
-            <img src={movie.poster} alt={movie.title} className="poster-image"/>
-        </div>
+        <Grid item className="movie-card" onClick={toDetails}>
+            <Card>
+                <CardActionArea>
+                    <h3>{movie.title}</h3>
+                    <CardMedia
+                        className={classes.media}
+                        image={movie.poster}
+                        title={movie.title}
+                    />
+                    {/* <img src={movie.poster} alt={movie.title} className="poster-image"/> */}
+                </CardActionArea>
+            </Card>
+        </Grid>
     )
 }
 
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,28 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import './MovieList.css'
+// import './MovieList.css'
 import MovieItem from '../MovieItem/MovieItem.jsx'
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+// import FormLabel from '@material-ui/core/FormLabel';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import RadioGroup from '@material-ui/core/RadioGroup';
+// import Radio from '@material-ui/core/Radio';
+// import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+}));
 
 function MovieList() {
+    const classes= useStyles();
+    const spacing = 3;
 
     const dispatch = useDispatch();
     const history = useHistory();
@@ -18,13 +36,13 @@ function MovieList() {
         <main>
             <h1>MovieList</h1>
             <button onClick={() => history.push('/addmovie')}>Add Movie!</button>
-            <section className="movies">
+            <Grid container justify="center" spacing={spacing}>
                 {movies.map(movie => {
                     return (
                         <MovieItem key={movie.id} movie={movie}/>
                     );
                 })}
-            </section>
+            </Grid>
         </main>
 
     );
